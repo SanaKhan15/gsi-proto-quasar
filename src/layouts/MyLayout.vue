fc6ff9aff031f69bdf790827adf1d4cb78fe3108
Sana Khan <sana.khan@cesltd.com>
2019-07-02 15:04:17 +0530

    fixed scroll bar in drawer section

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -7,8 +7,6 @@
           <span class="text-grey-9">GSI</span>
           <span class="text-grey-6 q-ml-sm">Health</span>
         </q-toolbar-title>
-        <!-- <q-btn flat round dense color="grey-8" icon="search" /> -->
-        <!-- <div class="row justify-center"> -->
 
         <q-toolbar-title class="q-mt-sm q-mb-sm q-mr-xl text-center">
           <q-input
@@ -24,64 +22,63 @@
             </template>
           </q-input>
         </q-toolbar-title>
-
-        <!-- </div> -->
       </q-toolbar>
     </q-header>
 
     <q-drawer
       v-model="drawer"
       :mini="!drawer || miniState"
-      @click.capture="drawerClick"
       :width="250"
-      :breakpoint="500"
       content-class="bg-indigo-6"
     >
-    
+      <q-item>
+        <q-item-section avatar>
+          <q-icon
+            color="white"
+            name="chevron_right"
+            v-if="miniState"
+            @click.stop="miniState = !miniState"
+          ></q-icon>
+        </q-item-section>
+      </q-item>
+      <q-item>
+        <q-avatar>
+          <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+        </q-avatar>
         <q-item>
+          <q-item-section class="text-white">John Leider</q-item-section>
+        </q-item>
+
+        <q-item-section avatar>
+          <q-icon
+            v-if="!miniState"
+            color="white"
+            name="chevron_left"
+            @click.stop="miniState = !miniState"
+          ></q-icon>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-btn v-if="miniState" round color="red-10" icon="add" />
+
+        <q-btn v-if="!miniState" color="red-10" icon="add" label="ADD PATIENT" />
+      </q-item>
+
+      <q-list v-for="item in items" :key="item.title">
+        <q-item clickable v-ripple>
           <q-item-section avatar>
-            <q-icon
-              color="white"
-              name="chevron_right"
-              v-if="miniState"
-              @click.stop="miniState = !miniState"
-            ></q-icon>
+            <q-icon :name="item.icon" color="white"></q-icon>
           </q-item-section>
+          <q-tooltip v-if="miniState" anchor="center right" self="center left" :offset="[10, 10]">
+            <strong>{{ item.title }}</strong>
+          </q-tooltip>
+          <q-item-section class="text-grey-1">{{ item.title }}</q-item-section>
         </q-item>
-        <q-item>
-          <q-avatar>
-            <img src="https://randomuser.me/api/portraits/men/85.jpg">
-          </q-avatar>
-          <q-item>
-            <q-item-section class="text-white">John Leider</q-item-section>
-          </q-item>
-
-          <q-item-section avatar>
-            <q-icon color="white" name="chevron_left" @click.stop="miniState = !miniState"></q-icon>
-          </q-item-section>
-        </q-item>
-
-        <q-item>
-          <q-btn v-if="miniState" round color="red-10" icon="add"/>
-
-          <q-btn v-if="!miniState" color="red-10" icon="add" label="ADD PATIENT"/>
-        </q-item>
-
-        <q-list v-for="item in items" :key="item.title">
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon :name="item.icon" color="white"></q-icon>
-            </q-item-section>
-            <q-tooltip v-if="miniState" anchor="center right" self="center left" :offset="[10, 10]">
-              <strong>{{ item.title }}</strong>
-            </q-tooltip>
-            <q-item-section class="text-grey-1">{{ item.title }}</q-item-section>
-          </q-item>
-        </q-list>
-      
+      </q-list>
     </q-drawer>
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
@@ -108,14 +105,9 @@ export default {
   },
   methods: {
     drawerClick(e) {
-      // if in "mini" state and user
-      // click on drawer, we switch it to "normal" mode
       if (this.miniState) {
         this.miniState = false;
 
-        // notice we have registered an event with capture flag;
-        // we need to stop further propagation as this click is
-        // intended for switching drawer to "normal" mode only
         e.stopPropagation();
       }
     }
@@ -123,4 +115,4 @@ export default {
 };
 </script>
 
-<style></style>
+
